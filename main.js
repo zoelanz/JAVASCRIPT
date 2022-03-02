9dd754f9e41851ffc9e17269f1ccd9a0f1e1a1c5
zoedesarrolloweb <zoe_lanz@hotmail.com>
2022-03-02 14:24:01 -0300

    arrays

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-// -------------------------DESAFIO 1----------------------------------//
+// -------------------------DESAFIO COMPLEMENTARIO : INCORPORAR ARRAYS----------------------------------//
 
-// -------------------SIMULADOR INTERACTIVO---------------------------//
 
 /* 
 1. INFORMAR CUANTO SALE CADA CUADRO
@@ -11,27 +10,42 @@
 4.SI NO INGRESA UN NUMERO VOLVER A PEDIR CANTIDAD DE CUADROS
 */
 
+// informo al usuario sobre las obras
 alert("Usted esta por comprar obras de la artista Merdeces Llanos!");
 alert("El valor por cuadro es de U$S1500, en caso elija 3 o mas sera de U$S1350 y en caso elija 5 o mas sera de U$S1250");
 
+// pido que ingrese la cantidad de cuadros que le gustaria al usuario comprar
 let cantidadCuadros = parseInt(prompt("Ingrese la cantidad de cuadros que desea comprar"))
-let precioCuadros = 1500;
-let precioCuadros2 = 1350;
-let precioCuadros3 = 1250;
+const precio = [];
+
+// agrego los precios al array
+precio.push(1500);
+precio.push(1350);
+precio.push(1250);
+
+// imprimo cantidad de precios y valores
+
+console.log(`Hay ${precio.length} precios, los cuales son: ${precio}`);
+
+// creo la funcion que le pasa el precio total 
 
 function operacion(a, b) {
     return a * b;
 }
 
+// inicio ciclo
+
 while (isNaN(cantidadCuadros) || cantidadCuadros <= 0) {
     cantidadCuadros = parseInt(prompt("Cantidad Erronea, Digite cantidad nuevamente"))
 }
 
+// condiciones del ciclo
+
 if (cantidadCuadros >= 0 && cantidadCuadros < 3) {
-    alert("El total es: " + operacion(cantidadCuadros, precioCuadros) + " dolares");
+    alert("El total es: " + operacion(cantidadCuadros, precio[0]) + " dolares");
 } else if (cantidadCuadros >= 3 && cantidadCuadros < 5) {
-    alert("El total es: " + operacion(cantidadCuadros, precioCuadros2) + " dolares");
+    alert("El total es: " + operacion(cantidadCuadros, precio[1]) + " dolares");
 } else if (cantidadCuadros >= 5) {
-    alert("El total es: " + operacion(cantidadCuadros, precioCuadros3) + " dolares");
+    alert("El total es: " + operacion(cantidadCuadros, precio[2]) + " dolares");
 }
 
